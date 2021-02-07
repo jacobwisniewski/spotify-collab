81de0a40b58077313f8675b59a3cae2282233371
Jacob Wisniewski <jacob.wisniewski@Jacobs-MacBook-Pro.local>
2021-02-08 10:11:39 +1100

    Fix spotify logo

---
+++ frontend/build/precache-manifest.2d8cc4e0ea3a5382d08dfd77f13158f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06259190226112633c56b9ef1b10113a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf04ebbcf2a2205c00d4",
+    "url": "/static/css/main.49626787.chunk.css"
+  },
+  {
+    "revision": "200a329c5a15b77cb093",
+    "url": "/static/js/2.a9e78b53.chunk.js"
+  },
+  {
+    "revision": "7a29fd67914f99c562dc3f65bbf1078b",
+    "url": "/static/js/2.a9e78b53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf04ebbcf2a2205c00d4",
+    "url": "/static/js/main.ce1f156f.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
